0b132cfccbce9baf42c5557e7032b295bdbb3f82
Gennady Telegin <gtelegin@gmail.com>
2013-07-19 15:47:42 +0400

    Use curl in bytehand

--- smsgates/bytehand.com/upload/system/smsgate/bytehand.php
+++ smsgates/bytehand.com/upload/system/smsgate/bytehand.php
@@ -1,8 +1,11 @@
 <?php
 final class Bytehand extends SmsGate {
-	public function send() {	
-		$results = array();		
-		$data = array(
+	private $baseurl = "http://bytehand.com:3800/";
+	
+	public function send() {
+		$ret = array();
+		
+		$params = array(
 			'id'	=> $this->username,
 			'key'	=> $this->password,
 			'from'	=> $this->from,
@@ -10,23 +13,44 @@ final class Bytehand extends SmsGate {
 			'text'	=> $this->message
 		);
 
-		$results[] = $this->process($data);
-
+		$ret[] = $this->request('send', $params);
+		
 		if ($this->copy) {
 			$numbers = explode(',', $this->copy);
 			foreach ($numbers as $number) {
-				$data['to']     = $number;
+				$params['to']     = $number;
 
-				$results[] = $this->process($data);
+				$ret[] = $this->request('send', $params);
 			}
 		}
-		return $results;
+		
+		return $ret;
 	}
-
-	private function process($data) {
-		$url = "http://bytehand.com:3800/send?". http_build_query($data);
-
-        return $response = @file_get_contents($url, false);
-   }
+   
+	private function request($method, $params) {
+		$url = $this->baseurl . $method;
+		
+		if (function_exists('curl_init')) {
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+			$buffer = curl_exec($ch);
+			curl_close($ch);
+		} else {
+			$context = stream_context_create(array(
+                'http' => array(
+                    'method' => 'POST',
+                    'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'content' => $params,
+                    'timeout' => 10,
+                ),
+            ));
+            $buffer = file_get_contents($url, false, $context);
+		}
+		
+		return $buffer;
+	}
 }
 ?>
